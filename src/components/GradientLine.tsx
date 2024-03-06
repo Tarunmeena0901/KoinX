5b164e21ac1517e5a9ae89d2bc00b3b627d7f7c6
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-03-06 22:01:49 +0530

    made performance section and a gradient line to display

--- src/components/GradientLine.tsx
+++ src/components/GradientLine.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
 
-const GradientLine = () => {
+
+const GradientLine = ({ value }) => {
   return (
-    <div className="container w-full h-2 relative">
-      <div className="line absolute inset-0 bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 animate-gradient rounded-lg"></div>
+    <div className="container relative">
+      {/* Arrow */}
+      <div className="arrow absolute top-0 left-1/2 w-0 h-0" style={{ marginLeft: "-5px", border: "5px solid transparent" }}></div>
+
+      {/* Gradient line */}
+      <div className="line bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 animate-gradient rounded-lg h-2"></div>
+
+      {/* Value */}
+      <div className="value absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white px-2 py-1 rounded shadow">
+        <span className="text-gray-900">{value}</span>
+      </div>
     </div>
   );
 };
-
 export default GradientLine;
---
+++ src/components/PerformanceSection.tsx
@@ -0,0 +1,59 @@
+import { Typography } from "@mui/material";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import GradientLine from "./GradientLine";
+
+export default function PerformanceSection() {
+    const [coinInfo, setCoinInfo] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1&interval=daily&precision=2`);
+                setCoinInfo(response.data);
+            } catch (error) {
+                console.error('Error fetching coin data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="w-full h-full">
+            <div className="mt-5 ml-10 mb-10">
+                <Typography style={{ fontSize: "26px", fontWeight: "bold", color: "black" }}>Performance</Typography>
+            </div>
+            <div className="grid grid-cols-5 gap-x-2 items-center justify-center h-full w-full">
+                <div className="flex flex-col items-center col-span-1 space-y-2">
+
+                    {/* the coingecko api is causing problem sometimes so intead 
+                    of using fetched api data , coinInfo.prices[0][1] we are using hard coded values */}
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "black" }}>Today's Low</Typography>
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "darkgray" }}>75,344.99</Typography>
+                </div>
+                <div className="flex justify-center col-span-3">
+                    <GradientLine value="75,344.99"/>
+                </div>
+                <div className="flex flex-col items-center col-span-1">
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "black" }}>Today's High</Typography>
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "darkgray" }}>77,234.92</Typography>
+                </div>
+            </div>
+            <div className="grid grid-cols-5 gap-x-2 items-center justify-center h-full w-full">
+                <div className="flex flex-col items-center col-span-1 space-y-2">
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "black" }}>4W_Low</Typography>
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "darkgray" }}>60,232.32</Typography>
+                </div>
+                <div className="flex justify-center col-span-3">
+                    <GradientLine value="75,344.99"/>
+                </div>
+                <div className="flex flex-col items-center col-span-1">
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "black" }}>4W_High</Typography>
+                    <Typography style={{ fontSize: "18px", fontWeight: "medium", color: "darkgray" }}>65,245.55</Typography>
+                </div>
+            </div>
+        </div>
+    )
+
+}
