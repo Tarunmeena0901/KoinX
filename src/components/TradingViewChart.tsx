d6dad599e7f1457099b06068c2a2cd83e8117415
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-03-11 03:15:07 +0530

    type error fix

--- src/components/TradingViewChart.tsx
+++ src/components/TradingViewChart.tsx
@@ -1,44 +1,43 @@
-// TradingViewWidget.jsx
-
 import { useRef, useEffect, memo } from "react";
 
-
 function TradingViewWidget() {
-  const container = useRef();
+  const container = useRef<HTMLDivElement>(null);
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "width": "100%",
-          "height": "610",
-          "symbol": "BITSTAMP:BTCUSD",
-          "interval": "D",
-          "timezone": "Etc/UTC",
-          "theme": "light",
-          "style": "2",
-          "locale": "en",
-          "enable_publishing": false,
-          "gridColor": "rgba(42, 46, 57, 0)",
-          "hide_top_toolbar": true,
-          "hide_legend": true,
-          "save_image": false,
-          "calendar": false,
-          "support_host": "https://www.tradingview.com"
-        }`;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "width": "100%",
+        "height": "610",
+        "symbol": "BITSTAMP:BTCUSD",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "2",
+        "locale": "en",
+        "enable_publishing": false,
+        "gridColor": "rgba(42, 46, 57, 0)",
+        "hide_top_toolbar": true,
+        "hide_legend": true,
+        "save_image": false,
+        "calendar": false,
+        "support_host": "https://www.tradingview.com"
+      }`;
+
+    if (container.current) {
       container.current.appendChild(script);
+    }
 
-      // Clean up function
-      return () => {
+    // Clean up function
+    return () => {
+      if (container.current) {
         container.current.removeChild(script);
-      };
-    },
-    []
-  );
+      }
+    };
+  }, []);
 
   return (
     <div className="tradingview-widget-container rounded-md w-full" ref={container}></div>
