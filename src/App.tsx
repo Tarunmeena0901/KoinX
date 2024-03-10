9beba2f48abbb78a95159455cfe48f69dd2b506f
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-03-11 03:40:24 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import MainPage from './pages/main-page';
 import NavBar from './components/NavBar';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
+      <Analytics/>
       <NavBar/>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
